21b69d7ab69059760adfdf6158be644c14f6a656
Cam Ellis <elliscj@colorado.edu>
2022-01-23 23:41:13 -0700

    adding images to assets folder. Card styling

--- client/src/components/pages/Projects.js
+++ client/src/components/pages/Projects.js
@@ -11,19 +11,21 @@ export default function Projects() {
       <div id="proj-cards">
         <Card className="col-md-5 col-sm-8 col-8 my-4">
           <Card.Img variant="top" src="./assets/watch-iT-screenshot.png" />
-          <Card.Body>
+          <Card.Body className="cards">
             <Card.Title>Watch-iT</Card.Title>
             <Card.Text>
               An app that allows you to easily create a watchlist, watch
               trailers, and share movie reviews.
             </Card.Text>
           </Card.Body>
-          {/* <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup> */}
-          <Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>
+              Key Technologies: Express.js, Node.js, SQL, Sequelize
+            </ListGroupItem>
+            {/* <ListGroupItem>Express.js, Node.js, SQL, Sequelize</ListGroupItem>
+            <ListGroupItem>TMDB</ListGroupItem> */}
+          </ListGroup>
+          <Card.Body className="cards">
             <Button
               variant="outline-dark"
               className="mx-2"
@@ -43,24 +45,24 @@ export default function Projects() {
           </Card.Body>
         </Card>
         <Card className="col-md-5 col-sm-8 col-8 my-4">
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Img
+            variant="top"
+            src="./assets/Screen Shot 2021-11-22 at 6.59.20 PM.png"
+          />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title>Brews of Interest</Card.Title>
+            <Card.Text>Recommendations for breweries in your area.</Card.Text>
           </Card.Body>
-          {/* <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup> */}
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Key Technologies: Open Brewery DB</ListGroupItem>
+            {/* <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem> */}
+          </ListGroup>
           <Card.Body>
             <Button
               variant="outline-dark"
               className="mx-2"
-              href="https://github.com/elliscj/watchIT"
+              href="https://github.com/elliscj/project-1"
               target="_blank"
             >
               Repo
@@ -68,7 +70,7 @@ export default function Projects() {
             <Button
               variant="outline-dark"
               className="mx-2"
-              href="https://thawing-fjord-57482.herokuapp.com/"
+              href="https://elliscj.github.io/project-1/"
               target="_blank"
             >
               Live Site
@@ -76,12 +78,11 @@ export default function Projects() {
           </Card.Body>
         </Card>
         <Card className="col-md-5 col-sm-8 col-8 my-4">
-          <Card.Img variant="top" src="./assets/watch-iT-screenshot.png" />
+          <Card.Img variant="top" src="./assets/weather-screenshot.png" />
           <Card.Body>
-            <Card.Title>Watch-iT</Card.Title>
+            <Card.Title>Weather Dashboard</Card.Title>
             <Card.Text>
-              An app that allows you to easily create a watchlist, watch
-              trailers, and share movie reviews.
+              Get current and forcasted weather for anywhere in the world!
             </Card.Text>
           </Card.Body>
           {/* <ListGroup className="list-group-flush">
@@ -93,7 +94,7 @@ export default function Projects() {
             <Button
               variant="outline-dark"
               className="mx-2"
-              href="https://github.com/elliscj/watchIT"
+              href="https://github.com/elliscj/hw6-weather-dashboard"
               target="_blank"
             >
               Repo
@@ -101,7 +102,7 @@ export default function Projects() {
             <Button
               variant="outline-dark"
               className="mx-2"
-              href="https://thawing-fjord-57482.herokuapp.com/"
+              href="https://elliscj.github.io/hw6-weather-dashboard/"
               target="_blank"
             >
               Live Site
